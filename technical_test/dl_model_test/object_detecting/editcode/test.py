0c112cb7cc9544e992a885d84eef7b0e4dc13f58
Jongmyung Kim <jongbob1918@gmail.com>
2025-06-18 07:56:58 +0900

    [IDS] opencv분류 로직 수정

--- technical_test/dl_model_test/object_detecting/editcode/test.py
+++ technical_test/dl_model_test/object_detecting/editcode/test.py
@@ -1,20 +1,21 @@
 from ultralytics import YOLO
 
 # 1. 모델 불러오기 (너가 파인튜닝한 best.pt 경로)
-model = YOLO("/home/mac/dev_ws/project/deeplearning-repo-2/technical_test/dl_model_test/object_detecting/single_model/yolo8s_seg/all_fine_fune_v1/weights/best.pt")
+model = YOLO("/home/mac/dev_ws/project/deeplearning-repo-2/IDS/models/yolov11seg-m_v2.pt")
 
 # 2. 테스트할 대상 경로
 # - 이미지: JPG/PNG
 # - 폴더: 폴더 경로
 # - 웹캠: 0
 # - 영상: MP4 경로
-source = "/home/mac/Videos/Screencasts/Screencast from 2025-06-03 16-31-44.webm"  # 웹캠이면 0 / 이미지 경로면 "C:/경로/파일.jpg"
+source = "/home/mac/Downloads/model_test_iphone_2.MOV"  # 웹캠이면 0 / 이미지 경로면 "C:/경로/파일.jpg"
+
 
 # 3. 추론 실행
 results = model.predict(
     source=source,
-    imgsz=2560,
-    conf=0.2,
+    imgsz=960,
+    conf=0.1,
     show=True,       # 화면에 바로 표시
     save=True        # runs/segment/predict에 저장
 )
